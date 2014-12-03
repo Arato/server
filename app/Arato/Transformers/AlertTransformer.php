2ed18656601ef4a2f053382ba64f2d559ef0c58e
Pierre BARON <prbaron22@gmail.com>
2014-12-03 17:58:52 +0100

    update date format

--- app/Arato/Transformers/AlertTransformer.php
+++ app/Arato/Transformers/AlertTransformer.php
@@ -7,12 +7,12 @@ class AlertTransformer extends Transformer
     public function transform($item)
     {
         return [
-            'id'        => $item['id'],
-            'title'     => $item['title'],
-            'content'   => $item['content'],
-            'price'     => $item['price'],
-            'user_id'   => $item['user_id'],
-            'createdAt' => $item['created_at']
+            'id'         => $item['id'],
+            'title'      => $item['title'],
+            'content'    => $item['content'],
+            'price'      => $item['price'],
+            'user_id'    => $item['user_id'],
+            'created_at' => $item['created_at']->toIso8601String()
         ];
     }
 }
