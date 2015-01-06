3d8949d4f390d4929e4228a96318890a39395387
Pierre BARON <prbaron22@gmail.com>
2015-01-06 19:00:17 +0100

    reminder password

--- app/controllers/RemindersController.php
+++ app/controllers/RemindersController.php
@@ -1,65 +1,76 @@
 <?php
+use Arato\Repositories\UserRepository;
+use controllers\ApiController;
+use Illuminate\Auth\Reminders\ReminderRepositoryInterface;
 
-class RemindersController extends Controller {
+class RemindersController extends ApiController
+{
+    protected $userRepository;
+    protected $reminders;
 
-	/**
-	 * Handle a POST request to remind a user of their password.
-	 *
-	 * @return Response
-	 */
-	public function remind()
-	{
-		switch ($response = Password::remind(Input::only('email')))
-		{
-			case Password::INVALID_USER:
-				return Redirect::back()->with('error', Lang::get($response));
+    function __construct(UserRepository $userRepository, ReminderRepositoryInterface $reminders)
+    {
+        $this->userRepository = $userRepository;
+        $this->reminders = $reminders;
+    }
 
-			case Password::REMINDER_SENT:
-				return Redirect::back()->with('status', Lang::get($response));
-		}
-	}
+    /**
+     * Handle a POST request to remind a user of their password.
+     *
+     * @return Response
+     */
+    public function remind()
+    {
+        switch ($response = Password::remind(Input::only('email'))) {
+            case Password::INVALID_USER:
+                return $this->setStatusCode(400)->respondWithError(Lang::get($response));
+            case Password::REMINDER_SENT:
+                return $this->respond("");
+        }
+    }
 
-	/**
-	 * Display the password reset view for the given token.
-	 *
-	 * @param  string  $token
-	 * @return Response
-	 */
-	public function show($token = null)
-	{
-		if (is_null($token)) App::abort(404);
+    /**
+     * Display the password reset view for the given token.
+     *
+     * @param  string $token
+     *
+     * @return Response
+     */
+    public
+    function getReset($token = null)
+    {
+        if (is_null($token)) App::abort(404);
 
-		return View::make('password.reset')->with('token', $token);
-	}
+        return View::make('password.reset')->with('token', $token);
+    }
 
-	/**
-	 * Handle a POST request to reset a user's password.
-	 *
-	 * @return Response
-	 */
-	public function reset()
-	{
-		$credentials = Input::only(
-			'email', 'password', 'password_confirmation', 'token'
-		);
+    /**
+     * Handle a POST request to reset a user's password.
+     *
+     * @return Response
+     */
+    public
+    function postReset()
+    {
+        $credentials = Input::only(
+            'email', 'password', 'password_confirmation', 'token'
+        );
 
-		$response = Password::reset($credentials, function($user, $password)
-		{
-			$user->password = Hash::make($password);
+        $response = Password::reset($credentials, function ($user, $password) {
+            $user->password = Hash::make($password);
 
-			$user->save();
-		});
+            $user->save();
+        });
 
-		switch ($response)
-		{
-			case Password::INVALID_PASSWORD:
-			case Password::INVALID_TOKEN:
-			case Password::INVALID_USER:
-				return Redirect::back()->with('error', Lang::get($response));
+        switch ($response) {
+            case Password::INVALID_PASSWORD:
+            case Password::INVALID_TOKEN:
+            case Password::INVALID_USER:
+                return Redirect::back()->with('error', Lang::get($response));
 
-			case Password::PASSWORD_RESET:
-				return Redirect::to('/');
-		}
-	}
+            case Password::PASSWORD_RESET:
+                return Redirect::to('/');
+        }
+    }
 
 }
