ac61cbe90457084cb97d09abb6219ad3feba58b2
Pierre Baron <prbaron22@gmail.com>
2015-01-31 21:34:53 +0100

    Create .env.php

---
+++ .env.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    'app'      => [
+        'key' => ''
+    ],
+    'database' => [
+        'host'     => '',
+        'database' => '',
+        'username' => '',
+        'password' => ''
+    ],
+    'mail'     => [
+        'address' => '',
+        'name'    => ''
+    ],
+    'smtp'     => [
+        'host'     => '',
+        'port'     => 587,
+        'username' => '',
+        'password' => '',
+    ]
+];
