49c575e849f1862606e74e5359ac8033dd603a57
Pierre Baron <prbaron22@gmail.com>
2015-01-31 23:22:45 +0100

    Update .env.php

--- .env.php
+++ .env.php
@@ -1,22 +1,17 @@
 <?php
 return [
-    'app'      => [
-        'key' => ''
-    ],
-    'database' => [
-        'host'     => '',
-        'database' => '',
-        'username' => '',
-        'password' => ''
-    ],
-    'mail'     => [
-        'address' => '',
-        'name'    => ''
-    ],
-    'smtp'     => [
-        'host'     => '',
-        'port'     => 587,
-        'username' => '',
-        'password' => '',
-    ]
+    'app.key'           => '',
+
+    'database.host'     => '',
+    'database.database' => '',
+    'database.username' => '',
+    'database.password' => '',
+
+    'mail.address'      => '',
+    'mail.name'         => '',
+
+    'smtp.host'         => '',
+    'smtp.port'         => 587,
+    'smtp.username'     => '',
+    'smtp.password'     => '',
 ];
