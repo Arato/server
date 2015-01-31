152a2ffede201c641832ce1324cc541f8d8216c0
Pierre Baron <prbaron22@gmail.com>
2015-01-31 23:23:24 +0100

    Create .env.testing.php

---
+++ .env.testing.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'app.key'           => '',
+    'database.host'     => '',
+    'database.database' => '',
+    'database.username' => '',
+    'database.password' => '',
+    'mail.address'      => '',
+    'mail.name'         => '',
+    'smtp.host'         => '',
+    'smtp.port'         => 587,
+    'smtp.username'     => '',
+    'smtp.password'     => '',
+];
