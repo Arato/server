089a24a8d3f0d13dc8f04855f7995cd57779d0af
Pierre BARON <prbaron22@gmail.com>
2015-03-19 15:04:09 +0100

    added http origin relative for email

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -7,7 +7,7 @@
 <h2>Password Reset</h2>
 
 <div>
-    <p>To reset your password, complete this form: <a href="http://localhost:9000/#/password/reset/{{$token}}">http://localhost:9000/#/password/reset/{{$token}}</a>.
+    <p>To reset your password, complete this form: <a href="{{$_SERVER['HTTP_ORIGIN']}}/#/password/reset/{{$token}}">{{$_SERVER['HTTP_ORIGIN']}}/#/password/reset/{{$token}}</a>.
     </p>
 
     <p>This link will expire in {{ Config::get('auth.reminder.expire', 60) }} minutes.</p>
